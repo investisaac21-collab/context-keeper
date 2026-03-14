a3ddcfbcaa6236d2627008a07269c25d6641495f
investisaac21-collab <investisaac21@gmail.com>
2026-03-14 22:01:41 +0100

    Add/update lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,44 +2,4 @@ import { createClientComponentClient, createServerComponentClient } from '@supab
 import { cookies } from 'next/headers'
 
 export const createClient = () => createClientComponentClient()
-
 export const createServerClient = () => createServerComponentClient({ cookies })
-
-export type Database = {
-  public: {
-    Tables: {
-      projects: {
-        Row: {
-          id: string
-          user_id: string
-          name: string
-          tag: string | null
-          context: string
-          copies: number
-          created_at: string
-          updated_at: string
-        }
-        Insert: {
-          id?: string
-          user_id: string
-          name: string
-          tag?: string | null
-          context: string
-          copies?: number
-          created_at?: string
-          updated_at?: string
-        }
-        Update: {
-          id?: string
-          user_id?: string
-          name?: string
-          tag?: string | null
-          context?: string
-          copies?: number
-          created_at?: string
-          updated_at?: string
-        }
-      }
-    }
-  }
-}
